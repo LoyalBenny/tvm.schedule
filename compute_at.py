96e6b0633441cabc89a102d82bff5baa31e7b4b5
LoyalBenny <38778964+LoyalBenny@users.noreply.github.com>
2021-07-29 17:18:42 +0800

    Update compute_at.py

--- compute_at.py
+++ compute_at.py
@@ -1,20 +1,12 @@
 import tvm
 
-n = 1024
-A = tvm.placeholder((n,), name='A')
-k = tvm.reduce_axis((0, n), 'k')
-B = tvm.compute((1,), lambda i: tvm.sum(A[k], axis=k), name='B')
+A = te.placeholder((m,), name="A")
+B = te.compute((m,), lambda i: A[i] + 1, name="B")
+C = te.compute((m,), lambda i: B[i] * 2, name="C")
+print(tvm.lower(s, [A, B, C], simple_mode=True))
 
-s = tvm.create_schedule(B.op)
-ko, ki = s[B].split(B.op.reduce_axis[0], factor=32)
-BF = s.rfactor(B, ki)
-
-tx = tvm.thread_axis("threadIdx.x")
-s[B].bind(s[B].op.reduce_axis[0], tx)
-
-print(tvm.lower(s, [A, B], simple_mode=True))
 print("---------cutting line---------")
 
-s[BF].compute_at(s[B], s[B].op.reduce_axis[0])
-
-print(tvm.lower(s, [A, B], simple_mode=True))
+s = te.create_schedule(C.op)
+s[B].compute_at(s[C], C.op.axis[0])
+print(tvm.lower(s, [A, B, C], simple_mode=True))
